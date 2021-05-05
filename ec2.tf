4e898e83e705be32e89b2cddd22ea477654f96c3
Joshua Clifford <82784160+jbclifford94@users.noreply.github.com>
2021-05-05 13:30:18 -0600

    Revert "Merge pull request #1 from jbclifford94/TF_AMI"
    This reverts commit d48a62f1d45b8908d6af0ecd891cf5775fcb0db5, reversing
    changes made to eb66693ad61ebbeff85d26a6f2b626f2e2649b66.

--- ec2.tf
+++ ec2.tf
@@ -1,23 +1,5 @@
-resource "aws_ami" "web1" {
-  name = "JCEAWeb1"
-
-  ebs_block_device {
-    device_name = "/dev/sdh"
-    snapshot_id = "snap-050ca58b45223c217"
-  }
-}  
-
-resource "aws_ami" "web2" {
-  name = "JCEAWeb2"
-
-  ebs_block_device {
-    device_name = "/dev/sdh"
-    snapshot_id = "snap-01c879bc06f286b19"
-  }
-}  
-
 resource "aws_instance" "web1" {
-  ami               = "web1"
+  ami               = "ami-0622eb223ad85775e" # us-east-2
   instance_type     = "t2.micro"
   availability_zone = "us-east-2a"
   key_name          = "wub-aws"
@@ -29,7 +11,7 @@ resource "aws_instance" "web1" {
 }
 
 resource "aws_instance" "web2" {
-  ami               = "web2"
+  ami               = "ami-0622eb223ad85775e" # us-east-2
   instance_type     = "t2.micro"
   availability_zone = "us-east-2c"
   key_name          = "wub-aws"
