216990eb828a8e9dbb1d7fe46ba794dee88558c4
Eric Ashenden <eashenden93@gmail.com>
2021-06-26 20:33:28 -0600

    Update VPC.tf
    Removed ec2 instance and security group as they were not needed for this implementation

--- VPC.tf
+++ VPC.tf
@@ -27,33 +27,3 @@ resource "aws_route_table_association" "rta_subnet_public" {
   subnet_id      = aws_subnet.subnet_public.id
   route_table_id = aws_route_table.rtb_public.id
 }
-
-resource "aws_security_group" "sg_22" {
-  name   = "sg_22"
-  vpc_id = aws_vpc.vpc.id
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_key_pair" "ec2key" {
-  key_name   = "publicKey"
-  public_key = "/home/wub/projects/github/AWS_WebLB/terraform.tfvars"
-}
-
-resource "aws_instance" "testInstance" {
-  ami                    = "ami-0622eb223ad85775e"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.subnet_public.id
-  vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
-  key_name               = aws_key_pair.ec2key.key_name
-}
