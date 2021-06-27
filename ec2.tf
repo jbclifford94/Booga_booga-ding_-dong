b874e96b183039b1ff1daf79c42b919c5a3bd226
Eric Ashenden <eashenden93@gmail.com>
2021-06-27 14:26:23 -0600

    Update ec2.tf
    Updated web2 subnet_id, the AZ was the same as web1 (needs to be a different one for the LB)

--- ec2.tf
+++ ec2.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "web2" {
   ami               = data.aws_ami.ubuntu.id # us-east-2
   instance_type     = "t2.micro"
   key_name          = "wub-aws"
-  subnet_id       = aws_subnet.subnet_use2a_public.id
+  subnet_id       = aws_subnet.subnet_use2c_public.id
   security_groups   = [aws_security_group.ssh_anywhere.id]
   tags = {
     Name = "web2"
